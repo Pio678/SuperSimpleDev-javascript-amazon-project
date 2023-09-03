8cabecf9fc0dbd81625f1fe021fde8d544d59c2d
Piotr Juszczak <piort789@gmail.com>
2023-09-03 17:33:14 +0200

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,15 @@
 //export means variables can be imported by another file
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 3,
-  },
-];
+//we load cart from local storage
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (cart === null || cart === undefined) {
+  cart = [];
+}
+
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
+
 export function addToCart(productId) {
   let matchingItem = null;
 
@@ -29,6 +30,8 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -43,5 +46,7 @@ export function removeFromCart(productId) {
     }
     // we override cart with newCart
     cart = newCart;
+
+    saveToStorage();
   });
 }
